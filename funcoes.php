074e347b4b2d12e45d5bd9f41eb812ffe89724e8
danilo.azevedosanti <danilo.azevedosanti@gmail.com>
2019-04-25 15:04:38 -0300

    SM - inclusão da operação edite e delete. MySql

--- funcoes.php
+++ funcoes.php
@@ -1,12 +1,28 @@
+<meta http-equiv="Content-Type" content="text/html" charset="utf-8" />
 <?php
 include "conexaoBD.php";
-include "form.php";
+include "index.php";
 $grava_nome = $_POST['nome'];
 $grava_email = $_POST['email'];
 $grava_msg = $_POST['msg'];
+//###################################################
+if($_GET['funcao'] == "gravar"){
+	$sql_gravar = mysqli_query($db, "insert into tb_curso (nome, email, mensagem) value ('$grava_nome','$grava_email','$grava_msg')");
+	header('Location:index.php');
+}
 
-	if($_GET['funcao'] == "gravar")
-	{
-	$sql_gravar= mysqli_query($db, "insert into tb_curso(nome, email, mensagem) values('$grava_nome', '$grava_email', '$grava_msg')");
-	header('Location:form.php');
-	}
+//*******************************************************************
+
+if($_GET['funcao'] == "editar"){
+	$id = $_GET['id'];
+	mysqli_query($db, "update tb_curso set nome='$grava_nome', email='$grava_email', mensagem='$grava_msg' where id = '$id'");
+	header('Location:index.php');
+}
+
+//*******************************************************************
+
+if($_GET['funcao'] == "excluir"){
+	$id = $_GET['id'];
+	mysqli_query($db, "delete from tb_curso where id = '$id'");
+	header('Location:index.php');
+}
--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-BR">
 <head>
 </head>
 <body>
+	<?php
+	if(isset($_GET['funcao']) != "editar"){
+	?>
 	<form id="form1" method="post" action="funcoes.php?funcao=gravar">
 		<table>
 			<tr>
@@ -19,16 +23,65 @@
 			<tr>
 				<td>Mensagem:</td>
 				<td><label>
-					<textarea name="msg" type="textarea" id="msg" cols="40" rows="4"> </textarea>
+					<textarea name="msg" type="textarea" id="msg" cols="42" rows="4"></textarea>
 				</label></td>
 			</tr>
-			<td><label>
-				<input name="sub" type="submit" id="sub" value="Cadastrar"  />
-				</label></td>
+			<td></td>
 			</tr>
 		</table>
+		<br>
+		<label>
+			<input name="sub" type="submit" id="sub" value="Cadastrar"/>
+		</label>
 	</form>
+	<?php
+	}
+	?>
 
+<?php
+include 'conexaoBD.php';
+	if(isset($_GET['funcao']) == "editar"){
+	$id = $_GET['id'];	
+	$sql_update = mysqli_query($db, "select * from tb_curso where id = '$id'");
+	while($linha = mysqli_fetch_array($sql_update)){
+				$nome = $linha['nome'];
+				$email = $linha['email'];
+				$msg = $linha['mensagem'];
+			}
+?>
+<form id="form2" method="post" action="funcoes.php?funcao=editar&id=<?php echo $id?>">
+		<table>
+			<tr>
+				<td>Nome:</td>
+				<td><label>
+					<input name="nome" type="text" id="nome" size="40" value="<?php echo $nome ?>" />
+				</label></td>
+			</tr>
+			<tr>
+				<td>Email:</td>
+				<td><label>
+					<input name="email" type="text" id="email" size="40" value="<?php echo $email ?>"/>
+				</label></td>
+			</tr>
+			<tr>
+				<td>Mensagem:</td>
+				<td><label>
+					<textarea name="msg" type="textarea" id="msg" cols="40" rows="4"><?php echo htmlspecialchars($msg) ?></textarea>
+				</label></td>
+			</tr>
+			 <br>
+			<tr>
+			</tr>
+		</table>
+		<br>
+		<label>
+			<input name="sub" id="sub" type="submit" class="btn btn-default" value="Atualizar"/>
+		 	| <a href="index.php" class="btn btn-default"><button>Cancelar</button></a>
+		</label></td>
+	</form>
+<?php
+}
+?>
 	<br />
 	<table width="750" border="0" cellpadding="3" cellspacing="3">
 		<tr>
@@ -42,14 +95,14 @@
 		
 			$sql_listar = mysqli_query($db, "select * from tb_curso order by nome");
 			while($linha = mysqli_fetch_array($sql_listar)){
-				$pnome = $linha['nome'];
-				$pid = $linha['id'];
+				$nome = $linha['nome'];
+				$id = $linha['id'];
 		?>	
 		
 		<tr>
-			<td><?php echo $pnome ?></td>
-			<td align="center"><a href="funcoes.phpfuncao=editar&id=<?php echo $pid ?>">Editar</a></td>
-			<td align="center"><a href="funcoes.phpfuncao=remove&id=<?php echo $pid ?>">Remover</a></td>
+			<td><?php echo $nome ?></td>
+			<td align="center"><a href="index.php?funcao=editar&id=<?php echo $id ?>">Editar</a></td>
+			<td align="center"><a href="funcoes.php?funcao=excluir&id=<?php echo $id ?>">Remover</a></td>
 		</tr>
 		<?php
 			}
